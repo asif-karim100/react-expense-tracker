4fdff7693fe8dbe3bb100f2a2a84fe4df1dfb266
asifkarim <asif.karim100@gmail.com>
2023-10-20 23:50:23 +0530

    screen sign up page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,6 +23,7 @@ const Login = () => {
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCgRIRM-lxcgOr8ZlinFrQV2WU-V0AB_nQ";
 
       try {
+        
         const authData = {
           email: emailInput,
           password: passwordInput,
